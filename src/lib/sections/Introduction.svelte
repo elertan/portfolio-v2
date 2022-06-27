683b0bca10dba2d6eac5adc3c33b547707dedac2
Dennis Kievits <denkievits@gmail.com>
2022-06-27 23:20:30 +0200

    transition introduction

--- src/lib/sections/Introduction.svelte
+++ src/lib/sections/Introduction.svelte
@@ -1,23 +1,33 @@
 <script lang="ts">
     import Container from "../components/Container.svelte";
     import Section from "../components/Section.svelte";
+    import {fly, fade} from 'svelte/transition';
+    import {onMount} from "svelte";
+    import {cubicInOut} from "svelte/easing";
+
+    let ready = false;
+    onMount(() => ready = true);
 </script>
 
 <div class="root">
     <Section id="introduction">
         <Container>
-            <div class="container">
-                <!--                <img class="picture" src="https://denniskievits.com/me.jpg" alt="picture" />-->
-                <!--                <h1 class="name">Dennis Kievits</h1>-->
-                <!--                <p class="role">Software Engineer</p>-->
-
-                <p class="pre">Hello, my name is</p>
-                <h1 class="name">Dennis Kievits.</h1>
-                <p class="headline">And technology is what interests me.</p>
-                <p class="intro">I'm a Software Architect specializing in designing and building efficient software, without ever sacrificing the focus on
-                    an exceptional user experience.</p>
-                <p class="intro-2">At the moment, I’m focused on building software solutions for my clients at <a href="https://www.velachi.com">Velachi</a>.</p>
-            </div>
+            {#if ready}
+                <div class="container">
+                    <p class="pre" in:fade="{{ y: -100, duration: 500, easing: cubicInOut }}">Hello, my name is</p>
+                    <h1 class="name" in:fade="{{ duration: 500, easing: cubicInOut, delay: 600 }}">Dennis
+                        Kievits.</h1>
+                    <p class="headline" in:fade="{{ duration: 500, easing: cubicInOut, delay: 1500 }}">And
+                        technology is what interests me.</p>
+                    <p class="intro" in:fly="{{ y: 50, duration: 500, easing: cubicInOut, delay: 2500 }}">I'm a Software
+                        Architect specializing in designing and building efficient software,
+                        without ever sacrificing the focus on
+                        an exceptional user experience.</p>
+                    <p class="intro-2" in:fly="{{ y: 50, duration: 500, easing: cubicInOut, delay: 3250 }}">At the
+                        moment, I’m focused on building software solutions for my clients at <a
+                                href="https://www.velachi.com">Velachi</a>.</p>
+                </div>
+            {/if}
         </Container>
     </Section>
 </div>
