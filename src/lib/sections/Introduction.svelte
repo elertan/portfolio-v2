ae432a682574df8757748075157ed3bc9786d18c
Dennis Kievits <denkievits@gmail.com>
2022-06-27 22:56:17 +0200

    progress/styling

--- src/lib/sections/Introduction.svelte
+++ src/lib/sections/Introduction.svelte
@@ -13,7 +13,7 @@
 
                 <p class="pre">Hello, my name is</p>
                 <h1 class="name">Dennis Kievits.</h1>
-                <p class="headline">And technology is what drives me every day.</p>
+                <p class="headline">And technology is what interests me.</p>
                 <p class="intro">I'm a Software Architect specializing in designing and building efficient software, without ever sacrificing the focus on
                     an exceptional user experience.</p>
                 <p class="intro-2">At the moment, I’m focused on building software solutions for my clients at <a href="https://www.velachi.com">Velachi</a>.</p>
@@ -46,19 +46,19 @@
 
   .headline {
     margin-top: calc(1 * var(--spacing-unit));
-    font-size: calc(2.5 * var(--font-size));
+    font-size: calc(2.75 * var(--font-size));
     font-weight: bolder;
     //color: var(--color-secondary);
   }
 
   .intro {
-    margin-top: calc(2 * var(--spacing-unit));
+    margin-top: calc(2.5 * var(--spacing-unit));
     font-size: calc(1.1 * var(--font-size));
     color: var(--color-text-muted);
   }
 
   .intro-2 {
-    margin-top: calc(.25 * var(--spacing-unit));
+    margin-top: calc(.3 * var(--spacing-unit));
     font-size: calc(1.1 * var(--font-size));
     color: var(--color-text-muted);
   }
