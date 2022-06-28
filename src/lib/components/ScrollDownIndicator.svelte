db8e103c4b0923a76c20f25303804f7bf360aec9
Dennis Kievits <denkievits@gmail.com>
2022-06-28 02:37:17 +0200

    scroll indicator muted

--- src/lib/components/ScrollDownIndicator.svelte
+++ src/lib/components/ScrollDownIndicator.svelte
@@ -6,15 +6,10 @@
     xmlns="http://www.w3.org/2000/svg"
     style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;"
   >
+    <path class="wheel" d="M123.359,79.775l0,72.843" />
     <path
-      id="wheel"
-      d="M123.359,79.775l0,72.843"
-      style="fill:none;stroke:#fff;stroke-width:20px;"
-    />
-    <path
-      id="mouse"
+      class="mouse"
       d="M236.717,123.359c0,-62.565 -50.794,-113.359 -113.358,-113.359c-62.565,0 -113.359,50.794 -113.359,113.359l0,143.237c0,62.565 50.794,113.359 113.359,113.359c62.564,0 113.358,-50.794 113.358,-113.359l0,-143.237Z"
-      style="fill:none;stroke:#fff;stroke-width:20px;"
     />
   </svg>
   <p>Scroll down</p>
@@ -38,11 +33,17 @@
       transform: translateY(0);
     }
     30% {
-      transform: translateY(100px);
+      transform: translateY(80px);
     }
   }
 
-  svg #wheel {
+  svg .wheel {
     animation: scroll ease 2.5s infinite;
+  }
+
+  svg > path {
+    stroke: var(--color-text-muted);
+    stroke-width: 20px;
+    fill: none;
   }
 </style>
